6692710b0991d1a856973e71b4766e0d363fbac3
manozghale <manozghale@gmail.com>
2025-08-06 15:43:46 +0545

    Initial Commit

---
+++ SetFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SetFlix
+//
+//  Created by Manoj on 06/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SetFlixUITests/SetFlixUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SetFlixUITestsLaunchTests.swift
+//  SetFlixUITests
+//
+//  Created by Manoj on 06/08/2025.
+//
+
+import XCTest
+
+final class SetFlixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
